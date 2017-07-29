1460f0e52d2a1ee49378631c0613a6498b3c6522
Colin Marshall <marshallcolin71@gmail.com>
2017-07-28 20:21:47 -0400

    checkpoint 3 complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,29 @@
-angular.module('blocJams', []);
+angular.module('blocJams', ['ui.router']);
+
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled:true,   /*why use commas instead of semicolon here*/
+                requireBase: false
+
+            });
+
+
+}
+
+    angular
+        .module('blocJams', ['ui.router']);
+        .config(config);
+
+}) ();
+
+$stateProvider
+    .state('landing', {
+        url: '/',
+        templateUrl: '/templates/landing.html'
+    })
+    .state('album', {
+      url: '/album',
+      templateUrl: '/templates/album.html'
+    });
