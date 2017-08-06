97db2731e4adffb57d3403c75ee1b967573cd789
Colin Marshall <marshallcolin71@gmail.com>
2017-08-06 11:59:49 -0400

    checkpoint 8 complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,9 +1,17 @@
 (function () {
-    function SongPlayer () {
+    function SongPlayer (Fixtures) {
         var SongPlayer = {};
+        var currentAlbum = Fixtures.getAlbum();
 
-        var currentSong = null;
-
+        /**
+        @function getSongIndex
+        @desc gets index number of active song
+        @type {Object}
+        @return number
+        */
+        var getSongIndex = function(song) {
+            return currentAlbum.songs.indexOf(song);
+};
 
 /**
 * @desc Buzz object audio file
@@ -20,7 +28,7 @@
         var setSong = function(song) {
              if (currentBuzzObject) {
                  currentBuzzObject.stop();
-                 currentSong.playing = null;
+                 SongPlayer.currentSong.playing = null;
              }
 
              currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -28,24 +36,53 @@
                  preload: true
              });
 
-             currentSong = song;
+             SongPlayer.currentSong = song;
           };
 
-        SongPlayer.play = function(song) {
+        SongPlayer.SongPlayer.currentSong = null;  //Why is this repeated here as stated in cp 8?//
 
-            if (currentSong !== song) {
+        /**
+        @function .play
+        @desc Plays song selected if no song is SongPlayer.currentSong is null or if SongPlayer.currentSong is paused, then sets new song with setSong
+        @param {Object} song
+        */
+        SongPlayer.play = function(song) {
+            song = song || SongPlayer.currentSong;
+            if (SongPlayer.currentSong !== song) {
 
             setSong(song);
 
             playSong (song);
-
         }
-
       };
 
+      /**
+       @function .pause
+       @desc Pauses SongPlayer.currentSong, then sets song.playing to false
+       @param {Object} song
+       */
       SongPlayer.pause = function(song) {
+            song = song || SongPlayer.currentSong; //What exactly is the purpose of adding this line?
             currentBuzzObject.pause();
             song.playing = false;
+      };
+
+      /**
+        @function .previous
+        @desc finds index of active song and decrements it by one, change active song to new song index
+        */
+      SongPlayer.previous = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
+
+            if (currentSongIndex < 0) {
+                currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song;)
+            }
       };
 
       function playSong (song)  {
@@ -62,6 +99,6 @@
 
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
+        .factory('SongPlayer',['Fixtures', SongPlayer]);
 
 }) ();
